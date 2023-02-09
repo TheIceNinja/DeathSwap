3e014b832075063c3db417435ef46d5633e794c9
IceNinja <theiceninjalng@gmail.com>
2023-02-09 20:42:34 +0200

    Plugin is ready!

--- src/main/java/net/theiceninja/deathswap/game/states/ActiveGameState.java
+++ src/main/java/net/theiceninja/deathswap/game/states/ActiveGameState.java
@@ -25,6 +25,7 @@ public class ActiveGameState extends GameState {
     public void onEnable(DeathSwapPlugin plugin) {
         super.onEnable(plugin);
         getGame().sendMessage("&aהמשחק התחיל!");
+
         int randomNumber = (int) getGame().randomizer(39, (60 * 5) + 1);
         this.checkTask = new GameCheckTask(randomNumber, getGame());
         this.checkTask.runTaskTimer(plugin, 0, 20);
