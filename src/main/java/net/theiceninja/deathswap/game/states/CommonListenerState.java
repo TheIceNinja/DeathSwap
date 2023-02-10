bafc861f7614fabf9a040bf70e5625c23b3ae563
IceNinja <theiceninjalng@gmail.com>
2023-02-10 08:55:18 +0200

    Adding some comments and changing stuff

--- src/main/java/net/theiceninja/deathswap/game/states/CommonListenerState.java
+++ src/main/java/net/theiceninja/deathswap/game/states/CommonListenerState.java
@@ -3,6 +3,8 @@ package net.theiceninja.deathswap.game.states;
 import lombok.AllArgsConstructor;
 import net.theiceninja.deathswap.game.Game;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +12,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 @AllArgsConstructor
 public class CommonListenerState implements Listener {
@@ -48,5 +51,16 @@ public class CommonListenerState implements Listener {
         if (game.getGameState() instanceof ActiveGameState) return;
 
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    private void onInteract(PlayerInteractEvent event) {
+        if (game.getGameState() instanceof ActiveGameState) return;
+
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+
+        if (block.getType().equals(Material.CHEST))
+            event.setCancelled(true);
     }
 }
